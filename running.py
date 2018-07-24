cfd77a91628840be02036fbe5318102293e11124
u2015441 <ali.gillani@arbisoft.com>
2018-07-24 15:38:53 +0500

    first commit

---
+++ running.py
@@ -0,0 +1,17 @@
+class Person:
+  def __init__(self,name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
+
+print("Enter your name:")
+x = input()
+print("Hello, " + x)
+
+a = "Hello, World!"
+b = a.split(",")
+print(b[0])
