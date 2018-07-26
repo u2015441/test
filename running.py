81d832930669ad23f76407861005512a489d7519
u2015441 <ali.gillani@arbisoft.com>
2018-07-26 12:30:06 +0500

    final

--- running.py
+++ running.py
@@ -1,8 +1,22 @@
+def listsum(numList):
+   if len(numList) == 1:
+        return numList[0]
+   else:
+        return numList[0] + listsum(numList[1:])
+
 def best_profit(mylist):
-  b=max(mylist)
-  return b
+    a = len(mylist)
+    j = 1
+    temp_list = []
+    for i in range(0, a, 2):
+
+        temp_list.append(mylist[j] - mylist[i])
+
+        j = j+2
+    c = listsum(temp_list)
+    return c
 
 
-print ("Give your input")
+print("Give your inputs")
 a = [int(x) for x in input().split(',')]
-print ("The maximum Profit =",best_profit(a))
+print("The maximum Profit =", best_profit(a))
