0fd15eb6665ca5ec698a84ffd3de1296cac87099
Syed Muhammad Ali Hassan <41623774+ali-gillani@users.noreply.github.com>
2018-08-03 12:23:29 +0500

    pythonic way

--- task1.py
+++ task1.py
@@ -1,17 +1,14 @@
-def search_word(x, my_list):
-    for y in my_list:
-        if y == x:
-            return 1
-        else:
-            return 0
+def search_word(element, my_list):
+    if element in my_list:
+        return True
+    else:
+        return False
 
 
 print("Enter The Value")
 value = input()
 my_list1 = ['Lahore', 'Pakistan', 'Karachi', 'Peshawar', 'Quetta']
-if search_word(value, my_list1):
-    print("True")
-else:
-    print("False")
+print(search_word(value, my_list1))
+
 
 
